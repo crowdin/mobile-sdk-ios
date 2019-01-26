658ba97bfc2c039db392619ad12a322af00e0420
Serhii Londar <serhii.londar@gmail.com>
2019-01-26 18:16:12 +0200

    Add firebase for testing localization

--- Example/CrowdinSDK/AppDelegate.swift
+++ Example/CrowdinSDK/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CrowdinSDK
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         CrowdinSDK.start()
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
