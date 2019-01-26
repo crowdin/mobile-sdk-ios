1197f0acda964501c9b6069adc95f6cba7082336
Serhii Londar <serhii.londar@gmail.com>
2019-01-26 16:03:51 +0200

    Move files into correct place

---
+++ CrowdinSDK/Classes/Extensions/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  CrowdinSDK
+//
+//  Created by Serhii Londar on 1/26/19.
+//  Copyright © 2019 Crowdin. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+}
+
