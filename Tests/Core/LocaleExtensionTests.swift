5b488289fd0795a55947fe57901f5de7a70a4ce4
Serhii Londar <serhii.londar@gmail.com>
2019-10-08 19:09:56 +0300

    Create LocaleExtensionTests.swift

---
+++ Tests/Core/LocaleExtensionTests.swift
@@ -0,0 +1,23 @@
+//
+//  StringParamTests.swift
+//  CrowdinSDK-Unit-CrowdinSDK_Tests
+//
+//  Created by Serhii Londar on 10/4/19.
+//
+
+import XCTest
+@testable import CrowdinSDK
+
+class LocaleExtensionTests: XCTestCase {
+	override func setUp() {
+		super.setUp()
+//		continueAfterFailure = false
+//		XCUIApplication().launchArguments += ["-AppleLanguages", "(fr)"]
+//		XCUIApplication().launchArguments += ["-AppleLocale", "fr_FR"]
+//		XCUIApplication().launch()
+	}
+	
+	func testPreferredLocalizations() {
+		XCTAssert(Locale.preferredLocalizations.contains("en"), "Should containg en localization")
+	}
+}
