cfc98a81201a783625a8816fa8f08f182123b650
Serhii <serhii.londar@gmail.com>
2019-10-28 23:47:06 +0200

    Add Localization Extractor tests
    Make CrowdinLogin config throw exception on initialization with incorrect data.

--- Tests/Tests/CrowdinLoginConfigTests.swift
+++ Tests/Tests/CrowdinLoginConfigTests.swift
@@ -9,23 +9,23 @@ class CrowdinLoginConfigTests: XCTestCase {
     }
     
     func testLoginConfigInit() {
-        loginConfig = CrowdinLoginConfig(clientId: "clientId", clientSecret: "clientSecret", scope: "scope", redirectURI: "crowdintest")
+        loginConfig = CrowdinLoginConfig(clientId: "clientId", clientSecret: "clientSecret", scope: "scope", redirectURI: "crowdintest://")
         
         XCTAssertNotNil(loginConfig)
         XCTAssert(loginConfig.clientId == "clientId")
         XCTAssert(loginConfig.clientSecret == "clientSecret")
         XCTAssert(loginConfig.scope == "scope")
-        XCTAssert(loginConfig.redirectURI == "crowdintest")
+        XCTAssert(loginConfig.redirectURI == "crowdintest://")
     }
     
     func testLoginConfigOrganizationInit() {
-        loginConfig = CrowdinLoginConfig(clientId: "clientId", clientSecret: "clientSecret", scope: "scope", redirectURI: "crowdintest", organizationName: "organizationName")
+        loginConfig = CrowdinLoginConfig(clientId: "clientId", clientSecret: "clientSecret", scope: "scope", redirectURI: "crowdintest://", organizationName: "organizationName")
         
         XCTAssertNotNil(loginConfig)
         XCTAssert(loginConfig.clientId == "clientId")
         XCTAssert(loginConfig.clientSecret == "clientSecret")
         XCTAssert(loginConfig.scope == "scope")
-        XCTAssert(loginConfig.redirectURI == "crowdintest")
+        XCTAssert(loginConfig.redirectURI == "crowdintest://")
         XCTAssert(loginConfig.organizationName == "organizationName")
     }
 }
