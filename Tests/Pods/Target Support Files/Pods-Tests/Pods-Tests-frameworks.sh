6af58f093f02db9148cdb89c5e184479f798cdbf
Serhii Londar <serhii.londar@gmail.com>
2019-12-05 11:02:58 +0200

    Update tests project

--- Tests/Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
+++ Tests/Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BaseAPI/BaseAPI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BaseAPI/BaseAPI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
