c49e451287a27de063f58559471eb99804e35188
Serhii Londar <serhii.londar@gmail.com>
2019-12-09 18:14:37 +0200

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,15 @@
 // Dangerfile.swift
 
+import Foundation
 import Danger
-import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
+import DangerSwiftLint // package: https://github.com/ashfurrow/DangerSwiftLint.git
+
+let danger = Danger()
+
+warn("Danger Swift is working")
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
 
 SwiftLint.lint(directory: "CrowdinSDK/Classes", configFile: "CrowdinSDK/Classes/.swiftlint.yml")
