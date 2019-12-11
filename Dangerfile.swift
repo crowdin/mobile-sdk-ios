d3a8373f10c89b4362771270af444eacb1f11d3b
Serhii Londar <serhii.londar@gmail.com>
2019-12-11 16:31:41 +0200

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -9,4 +9,4 @@ if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.d
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-SwiftLint.lint(directory: "CrowdinSDK/Classes", configFile: "CrowdinSDK/Classes/.swiftlint.yml")
+SwiftLint.lint(directory: "CrowdinSDK/", configFile: "CrowdinSDK/Classes/.swiftlint.yml")
