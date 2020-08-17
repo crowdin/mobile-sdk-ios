27dd658f732dcda11f269a0b747c480b51f623fe
Serhii <serhii.londar@gmail.com>
2020-08-18 02:09:22 +0300

    Remove Objcetive-C example project.
    Move Objective-C example to Example project.

---
+++ Example/CrowdinSDK/ObjectiveCExample/ObjectiveCExample.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveCExample.h
+//  CrowdinSDK_Example
+//
+//  Created by Serhii Londar on 18.08.2020.
+//  Copyright © 2020 Crowdin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjectiveCExample : NSObject
+
+- (void)startSDK;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/CrowdinSDK/ObjectiveCExample/ObjectiveCExample.m
@@ -0,0 +1,33 @@
+//
+//  ObjectiveCExample.m
+//  CrowdinSDK_Example
+//
+//  Created by Serhii Londar on 18.08.2020.
+//  Copyright © 2020 Crowdin. All rights reserved.
+//
+
+#import "ObjectiveCExample.h"
+@import CrowdinSDK;
+
+@implementation ObjectiveCExample
+
+- (void)startSDK {
+    CrowdinProviderConfig *crowdinProviderConfig = [[CrowdinProviderConfig alloc] initWithHashString:@"53376706833043f14491518106i" sourceLanguage:@"en"];
+    CrowdinSDKConfig *config = [[CrowdinSDKConfig config] withCrowdinProviderConfig:crowdinProviderConfig];
+    [CrowdinSDK startWithConfig:config];
+    
+    __block NSInteger downloadHandlerId = [CrowdinSDK addDownloadHandler:^{
+        [CrowdinSDK removeDownloadHandler:downloadHandlerId];
+    }];
+    
+    __block NSInteger errorHandlerId = [CrowdinSDK addErrorUpdateHandler:^(NSArray<NSError *> * _Nonnull errors) {
+        for (NSError *error in errors) {
+            NSLog(@"%@", error.localizedDescription);
+        }
+        [CrowdinSDK removeErrorHandler:errorHandlerId];
+    }];
+    
+    
+}
+
+@end
