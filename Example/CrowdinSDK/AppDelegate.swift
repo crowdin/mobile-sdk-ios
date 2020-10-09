5cecdc715b368c2354460af254fea30f2881a5f8
Serhii <serhii.londar@gmail.com>
2020-10-10 02:25:58 +0300

    Update AppDelegate.swift
    Remove commented code
    Remove changing language

--- Example/CrowdinSDK/AppDelegate.swift
+++ Example/CrowdinSDK/AppDelegate.swift
@@ -15,28 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Setup only crowdin provider:
-//        let crowdinProviderConfig = CrowdinProviderConfig(hashString: "1c2f58c7c711435295d2408106i", stringsFileNames: ["/%osx_locale%/Localizable.strings"], pluralsFileNames: ["Localizable.stringsdict"], localizations: ["en", "de"], sourceLanguage: "en")
-//        CrowdinSDK.startWithConfig(CrowdinSDKConfig.config().with(crowdinProviderConfig: crowdinProviderConfig))
-
-        
-        
-        // Setup CrowdinSDK with crowdin sdk with all features for enterprise:
-//        let crowdinProviderConfig = CrowdinProviderConfig(hashString: "f78819e9fe3a5fe96d2a383b2ozt",
-//                                                          localizations: ["en", "de", "uk"],
-//                                                          sourceLanguage: "en")
-//        let loginConfig = try! CrowdinLoginConfig(clientId: "XjNxVvoJh6XMf8NGnwuG",
-//                                                  clientSecret: "Dw5TxCKvKQQRcPyAWEkTCZlxRGmcja6AFZNSld6U",
-//                                                  scope: "project.screenshot",
-//                                                  redirectURI: "crowdintest://",
-//                                                  organizationName: "serhiy")
-//        let crowdinSDKConfig = CrowdinSDKConfig.config().with(crowdinProviderConfig: crowdinProviderConfig)
-//                                                        .with(screenshotsEnabled: true)
-//														  .with(loginConfig: loginConfig)
-//                                                        .with(settingsEnabled: true)
-//                                                        .with(realtimeUpdatesEnabled: true)
-//        CrowdinSDK.startWithConfig(crowdinSDKConfig)
-		
 		// Setup CrowdinSDK with crowdin sdk with all features:
 		let crowdinProviderConfig = CrowdinProviderConfig(hashString: "2db137daf26d22bf499c998106i",
 														  sourceLanguage: "en")
@@ -48,24 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                         .with(settingsEnabled: true)
                                                         .with(realtimeUpdatesEnabled: true)
                                                         .with(screenshotsEnabled: true)
-        CrowdinSDK.startWithConfig(crowdinSDKConfig, completion: {
-            CrowdinSDK.mode = .customSDK
-            CrowdinSDK.currentLocalization = "es"
-        })
-        
-//        print(CrowdinSDK.localizationDictionary(for: "en"))
-        
-//		Setup CrowdinSDK with Info.plist. Initializes only localization delivery feature.
-//        CrowdinSDK.start()
-        
-        _ = CrowdinSDK.addDownloadHandler {
-            print("Localization downloaded")
-        }
-//
-//        _ = CrowdinSDK.addErrorUpdateHandler { (errors) in
-//            print("Localization download failed with errors:")
-//            errors.forEach({ print($0.localizedDescription) })
-//        }
+        CrowdinSDK.startWithConfig(crowdinSDKConfig, completion: { })
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
