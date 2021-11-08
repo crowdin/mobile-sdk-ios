da31e2a90434c3d0847aef8c1259872265e14748
Serhii Londar <serhii.londar@gmail.com>
2021-11-09 01:25:50 +0200

    Fix pod validation

--- CrowdinSDK/Classes/Providers/Crowdin/LanguageResolver/LanguageResolver.swift
+++ CrowdinSDK/Classes/Providers/Crowdin/LanguageResolver/LanguageResolver.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-protocol CrowdinLanguage {
-    var id: String { get }
-    var name: String { get }
-    var twoLettersCode: String { get }
-    var threeLettersCode: String { get }
-    var locale: String { get }
-    var osxCode: String { get }
-    var osxLocale: String { get }
-}
-
-extension CrowdinLanguage {
-    var iOSLanguageCode: String {
-        return self.osxLocale.replacingOccurrences(of: "_", with: "-")
-    }
-}
-
 protocol LanguageResolver {
     /// Get crowdin language locale code for iOS localization code.
     /// - Parameter localization: iOS localization identifier. (List of all - Locale.availableIdentifiers).
