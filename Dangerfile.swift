15b19c8c03b351d086daaa1780dd662607638b71
Andrii Bodnar <andrii.bodnar@crowdin.com>
2023-01-31 12:05:01 +0200

    wip

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,8 +5,8 @@ import Danger
 
 let danger = Danger()
 
-if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10 {
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 30 {
     warn("Big PR, try to keep changes smaller if you can")
 }
 
-SwiftLint.lint(directory: "CrowdinSDK/", configFile: "CrowdinSDK/Classes/.swiftlint.yml")
+SwiftLint.lint(directory: "Sources/CrowdinSDK/", configFile: "Sources/CrowdinSDK/.swiftlint.yml")
