2a8efdf3a2fa66e08b46e7ab79e1dd1b773b298c
Serhii Londar <serhii.londar@gmail.com>
2024-02-21 10:30:16 +0100

    fix: Logs fixes (#265)

--- Sources/CrowdinSDK/Features/LoginFeature/CrowdinLoginConfig.swift
+++ Sources/CrowdinSDK/Features/LoginFeature/CrowdinLoginConfig.swift
@@ -25,13 +25,13 @@ private let URLSchemeDocumentationLink = "https://developer.apple.com/documentat
         try self.init(with: clientId, clientSecret: clientSecret, scope: scope, redirectURI: redirectURI)
     }
     
-    @available(*, deprecated)
+    @available(*, deprecated, message: "Please pass organizationName to CrowdinProviderConfig: CrowdinProviderConfig(hashString: distributionHash, sourceLanguage: sourceLanguage, organizationName: organizationName)")
     public convenience init(clientId: String, clientSecret: String, scope: String, organizationName: String? = nil) throws {
         guard let redirectURI = Bundle.main.urlSchemes?.first else { throw NSError(domain: "Application do not support any URL Scheme. To setup it, please check - \(URLSchemeDocumentationLink)", code: defaultCrowdinErrorCode, userInfo: nil) }
         try self.init(with: clientId, clientSecret: clientSecret, scope: scope, redirectURI: redirectURI, organizationName: organizationName)
     }
     
-    @available(*, deprecated)
+    @available(*, deprecated, message: "Please pass organizationName to CrowdinProviderConfig: CrowdinProviderConfig(hashString: distributionHash, sourceLanguage: sourceLanguage, organizationName: organizationName)")
     public convenience init(clientId: String, clientSecret: String, scope: String, redirectURI: String, organizationName: String? = nil) throws {
         try self.init(with: clientId, clientSecret: clientSecret, scope: scope, redirectURI: redirectURI, organizationName: organizationName)
     }
