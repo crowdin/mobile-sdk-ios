42e10fef627d0f4cdc1b99f12e211df79c3328b6
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-05-03 14:13:57 +0300

    chore: remove Toast (#280)

--- Sources/CrowdinSDK/Settings/SettingsView/SettingsView+UITableView.swift
+++ Sources/CrowdinSDK/Settings/SettingsView/SettingsView+UITableView.swift
@@ -12,7 +12,7 @@ import UIKit
 extension SettingsView {
     func setupCells() {
         cells = []
-        
+
         if let loginFeature = LoginFeature.shared {
             let logInItemView = SettingsItemView(frame: .zero)
             if !LoginFeature.isLogined {
@@ -23,13 +23,11 @@ extension SettingsView {
                             self?.reloadData()
                             self?.reloadUI()
                         }
-                        let message = "Successfully logined"
+                        let message = "Logged in"
                         CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .info, message: message))
-                        self?.showToast(message)
-                    }, error: { [weak self] error in
+                    }, error: { error in
                         let message = "Login error - \(error.localizedDescription)"
                         CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .error, message: message))
-                        self?.showToast(message)
                     })
                     self?.isHidden = false
                     self?.reloadData()
@@ -44,41 +42,42 @@ extension SettingsView {
             logInItemView.statusView.isHidden = false
             cells.append(logInItemView)
         }
-        
+
         let reloadItemView = SettingsItemView(frame: .zero)
-        reloadItemView.action = { [weak self] in
+        reloadItemView.action = {
             RefreshLocalizationFeature.refreshLocalization()
+
             let message = RealtimeUpdateFeature.shared?.enabled == true ? "Localization fetched from Crowdin project" : "Localization fetched from distribution"
-            self?.showToast(message)
+            CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .info, message: message))
         }
+
         reloadItemView.title = "Reload translations"
         cells.append(reloadItemView)
-        
+
         if LoginFeature.isLogined {
             if var feature = RealtimeUpdateFeature.shared {
                 let realTimeUpdateItemView = SettingsItemView(frame: .zero)
-                feature.error = { [weak self] error in
+                feature.error = { error in
                     let message = "Error while starting real-time preview - \(error.localizedDescription)"
                     CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .error, message: message))
-                    self?.showToast(message)
                 }
-                
+
                 feature.success = { [weak self] in
                     let message = "Successfully started real-time preview"
                     CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .info, message: message))
+
                     self?.reloadData()
                     guard RealtimeUpdateFeature.shared?.enabled == false else {
                         return
                     }
-                    self?.showToast(message)
                 }
                 feature.disconnect = { [weak self] in
                     let message = "Real-time preview disabled"
                     CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .info, message: message))
+
                     self?.reloadData()
-                    self?.showToast(message)
                 }
-                
+
                 realTimeUpdateItemView.action = {
                     feature.enabled = !feature.enabled
                     realTimeUpdateItemView.title = feature.enabled ? "Real-time on" : "Real-time off"
@@ -88,26 +87,25 @@ extension SettingsView {
                 realTimeUpdateItemView.statusView.isHidden = false
                 cells.append(realTimeUpdateItemView)
             }
-            
+
             if let feature = ScreenshotFeature.shared {
                 let screenshotItemView = SettingsItemView(frame: .zero)
-                screenshotItemView.action = { [weak self] in
-                    let message = "Successfully captured screenshot"
+                screenshotItemView.action = {
+                    let message = "Screenshot captured"
                     feature.captureScreenshot(name: String(Date().timeIntervalSince1970), success: {
                         CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .info, message: message))
-                        self?.showToast(message)
                     }, errorHandler: { (error) in
                         let message = "Error while capturing screenshot - \(error?.localizedDescription ?? "Unknown")"
                         CrowdinLogsCollector.shared.add(log: CrowdinLog(type: .error, message: message))
-                        self?.showToast(message)
                     })
                 }
+
                 screenshotItemView.title = "Capture screenshot"
                 screenshotItemView.statusView.isHidden = true
                 cells.append(screenshotItemView)
             }
         }
-        
+
         let logsItemView = SettingsItemView(frame: .zero)
         logsItemView.action = {
             let logsVC = CrowdinLogsVC()
@@ -121,7 +119,7 @@ extension SettingsView {
         logsItemView.title = "Logs"
         logsItemView.statusView.isHidden = true
         cells.append(logsItemView)
-        
+
         let stopItem = SettingsItemView(frame: .zero)
         stopItem.action = {
             CrowdinSDK.stop()
