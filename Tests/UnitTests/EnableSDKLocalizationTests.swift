db1af9601629ec0e58fe5e70c5f12e201ffd987a
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-05-03 15:15:41 +0300

    test: debug testManualSDKModeEnabled (#282)

--- Tests/UnitTests/EnableSDKLocalizationTests.swift
+++ Tests/UnitTests/EnableSDKLocalizationTests.swift
@@ -10,71 +10,68 @@ import XCTest
 @testable import CrowdinSDK
 
 class EnableSDKLocalizationTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let crowdinProviderConfig = CrowdinProviderConfig(hashString: "5290b1cfa1eb44bf2581e78106i",
                                                           sourceLanguage: "en")
         let crowdinSDKConfig = CrowdinSDKConfig.config().with(crowdinProviderConfig: crowdinProviderConfig)
-        
+
         CrowdinSDK.startWithConfig(crowdinSDKConfig, completion: { })
     }
-    
-    
+
     override func tearDown() {
         CrowdinSDK.removeAllDownloadHandlers()
         CrowdinSDK.deintegrate()
         CrowdinSDK.stop()
     }
-    
+
     func testAutoSDKModeEnabled() {
         CrowdinSDK.currentLocalization = nil
-        
+
         let expectation = XCTestExpectation(description: "Download handler is called")
-        
+
         _ = CrowdinSDK.addDownloadHandler {
-            XCTAssert(CrowdinSDK.currentLocalization == "en", "Shouuld auto detect current localization as en")
+            XCTAssert(CrowdinSDK.currentLocalization == "en", "Should auto detect current localization as en")
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 60.0)
     }
-    
+
     func testManualSDKModeEnabled() {
         CrowdinSDK.currentLocalization = "de"
         
         let expectation = XCTestExpectation(description: "Download handler is called")
         
         _ = CrowdinSDK.addDownloadHandler {
-            XCTAssert(CrowdinSDK.currentLocalization == "de", "Shouuld set current localization to de")
+            XCTAssert(CrowdinSDK.currentLocalization == "de", "Should set current localization to de")
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 60.0)
+        wait(for: [expectation], timeout: 120.0)
     }
-    
-    
+
     func testAutoBundleModeEnabled() {
         CrowdinSDK.currentLocalization = nil
-        
+
         let expectation = XCTestExpectation(description: "Download handler is called")
-        
+
         _ = CrowdinSDK.addDownloadHandler {
-            XCTAssert(CrowdinSDK.currentLocalization == "en", "Shouuld auto detect current localization as en")
+            XCTAssert(CrowdinSDK.currentLocalization == "en", "Should auto detect current localization as en")
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 60.0)
     }
-    
+
     func testManualBundleModeEnabled() {
         CrowdinSDK.currentLocalization = "de"
-        
+
         let expectation = XCTestExpectation(description: "Download handler is called")
-        
+
         _ = CrowdinSDK.addDownloadHandler {
-            XCTAssert(CrowdinSDK.currentLocalization == "de", "Shouuld set current localization to de")
+            XCTAssert(CrowdinSDK.currentLocalization == "de", "Should set current localization to de")
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 60.0)
     }
 }
-
