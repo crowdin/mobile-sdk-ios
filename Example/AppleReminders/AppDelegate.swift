eba2d68542f31676f9b8e506beac70d4aa651dff
Serhii Londar <serhii.londar@gmail.com>
2025-02-12 01:37:18 +0100

    Update AppDelegate.swift

--- Example/AppleReminders/AppDelegate.swift
+++ Example/AppleReminders/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         true
     }
 
