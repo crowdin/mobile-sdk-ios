1c4004578a2ae06294792d007fb03f9797e06448
Serhii Londar <serhii.londar@gmail.com>
2025-06-14 17:47:45 +0200

    Add back AnyLocalizationDataSource.

--- Sources/CrowdinSDK/CrowdinSDK/Localization/Provider/LocalizationDataSource.swift
+++ Sources/CrowdinSDK/CrowdinSDK/Localization/Provider/LocalizationDataSource.swift
@@ -14,6 +14,44 @@ protocol LocalizationDataSourceProtocol {
     func update(with values: Values)
 }
 
+class AnyLocalizationDataSource<T>: LocalizationDataSourceProtocol {
+    typealias Values = T
+    
+    private let accessQueue = DispatchQueue(label: "com.crowdin.AnyLocalizationDataSource.accessQueue", attributes: .concurrent)
+    
+    private let _findKey: (String) -> String?
+    private let _findValues: (String, String) -> [Any]?
+    private let _update: (T) -> Void
+    
+    init<DS: LocalizationDataSourceProtocol>(_ dataSource: DS) where DS.Values == T {
+        self._findKey = dataSource.findKey
+        self._findValues = dataSource.findValues
+        self._update = dataSource.update
+    }
+    
+    func findKey(for string: String) -> String? {
+        var result: String?
+        accessQueue.sync {
+            result = _findKey(string)
+        }
+        return result
+    }
+    
+    func findValues(for string: String, with format: String) -> [Any]? {
+        var result: [Any]?
+        accessQueue.sync {
+            result = _findValues(string, format)
+        }
+        return result
+    }
+    
+    func update(with values: T) {
+        accessQueue.async(flags: .barrier) {
+            self._update(values)
+        }
+    }
+}
+
 class StringsLocalizationDataSource: LocalizationDataSourceProtocol {
     typealias Values = [String: String]
     
